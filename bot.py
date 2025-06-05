ec2d0bd88bd6b1306b3bf7014f5184084bcee8ef
averycung <averycung@gmail.com>
2025-06-04 22:45:26 -0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,244 @@
+from dotenv import load_dotenv    # For guarding API token
+from db import add_bull, add_axiom, claim_chat, view, delcode, find_ax, find_bull, find_owner  
+import os
+import telebot    # Telegram API
+import base58     # For decrypting CA
+import mysql.connector  
+from telebot import types
+
+
+load_dotenv()  
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+
+#----------------#
+# Helper methods #
+#----------------#
+
+## helper boolean method to determine whether a string is a valid CA or not
+def isValidCA(address):
+    try:
+        decoded = base58.b58decode(address)
+        return len(decoded) == 32
+    except Exception:
+        return False
+
+
+## helper method to return CA in a given message block
+def findCA(msg):
+    x = msg.text
+    words = x.split()
+    for word in words:
+        word = word.strip()
+        if isValidCA(word):
+            return word
+    return False
+
+
+
+
+
+
+#-------------#
+# Bot filters #
+#-------------#
+
+
+
+## create custom validCA filter to filter for valid CAs in chat messages
+class IsValidCA(telebot.custom_filters.SimpleCustomFilter):
+    key ='is_CA'
+    @staticmethod
+    def check(message:telebot.types.Message):
+        x = message.text
+        words = x.split()
+        for word in words:
+            word = word.strip()
+            if isValidCA(word):
+                return True
+        return False
+    
+
+## add bot filter
+bot.add_custom_filter(IsValidCA())
+
+
+
+#------------------#
+# Message handlers #
+#------------------#
+
+
+
+## START COMMAND
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "*I\\'m Lambo\\.* If you are new to Lambo, please see /instructions\n \nIf you want to shill your referral links in chat, I\\'m your guy\\.  \n \nYou can control me by"\
+                 " sending these commands: \n \n*Codes:* \n/setcode \\- set your referral link\n/viewcode \\- view your code\n/deletecode \\- delete your code", parse_mode="MarkdownV2")
+
+
+#------------------#
+
+## HELP COMMAND
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message, "*Commands:* \n/instructions \n/setcode \n/viewcode \n/deletecode", parse_mode="MarkdownV2")
+
+#------------------#
+
+## INSTRUCTIONS COMMAND
+@bot.message_handler(commands=['instructions'])
+def instructions(message):
+    bot.reply_to(message, "*Instructions:* \n*1\\.* Set your referral codes with */setcode*\\. Your referral code will be stored in my database"\
+                 " \n\n*2\\.* Use the */claim* command in a groupchat you would like your referrals to be shilled in\\. Once you claim a group chat, "\
+                 "I will respond to every CA posted in that group chat with your referral links\\. *note* you must be an admin to use */claim* in a chat"\
+                 "\n\n*3\\.* Update, view or delete your links with */setcode* , */viewcode* , or */deletecode*", parse_mode="MarkdownV2")
+
+
+
+#------------------#
+
+## SET CODE COMMAND
+user_codes = {}  
+@bot.message_handler(commands=['setcode'])
+def set_link(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    btn1 = types.KeyboardButton("BullX")
+    btn2 = types.KeyboardButton("Axiom")
+    markup.add(btn1, btn2)
+
+    msg = bot.reply_to(
+        message,
+        "Which platform would you like to set a code for?\n\nChoose an option:",
+        reply_markup=markup
+    )
+    bot.register_next_step_handler(msg, paste_ref_code)
+
+def paste_ref_code(message):
+    if message.text.lower() == "bullx":
+        msg1 = bot.reply_to(message, "Please paste your BullX referral code")
+        bot.register_next_step_handler(msg1, save_ref_code_bull)
+    elif message.text.lower() == "axiom":
+        msg2 = bot.reply_to(message, "Please paste your Axiom referral code")
+        bot.register_next_step_handler(msg2, save_ref_code_axiom)
+
+def save_ref_code_bull(message):
+    code = message.text
+    add_bull(message.from_user.id, code) 
+    bot.reply_to(message, "Your code has been updated!")
+
+def save_ref_code_axiom(message):
+    code = message.text
+    add_axiom(message.from_user.id, code) 
+    bot.reply_to(message, "Your code has been updated!")
+    
+
+
+#------------------#
+
+## VIEW CODE COMMAND
+@bot.message_handler(commands=['viewcode'])
+def view_link(message):
+    id = message.from_user.id
+    result = view(id)
+    if result:
+        bull, axiom = result
+        reply = f"🐂 *BullX:* `{bull}`\n🧪 *Axiom:* `{axiom}`"
+    else:
+        reply = "❌ You haven't set a referral code yet. Use /setcode to get started."
+    
+    bot.reply_to(message, reply, parse_mode="MarkdownV2")
+
+
+#------------------#
+
+## DELETE CODE COMMAND
+@bot.message_handler(commands=['deletecode'])
+def del_code(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    btn1 = types.KeyboardButton("BullX")
+    btn2 = types.KeyboardButton("Axiom")
+    markup.add(btn1, btn2)
+
+    msg = bot.reply_to(
+        message,
+        "Which code would you like to delete?\n\nChoose an option:",
+        reply_markup=markup
+    )
+    bot.register_next_step_handler(msg, delete)
+
+
+def delete(message):
+    id = message.from_user.id
+    if message.text.lower() == "bullx":
+        delcode(id, "bull_ref_code")
+        bot.reply_to(message, "Your BullX code has been deleted.")
+    elif message.text.lower() == "axiom":
+        delcode(id, "axiom_ref_code")
+        bot.reply_to(message, "Your Axiom code has been deleted.")
+
+#------------------#
+
+## CLAIM CHAT COMMAND
+
+@bot.message_handler(commands=['claim'])
+def claim(message):
+    id = message.from_user.id
+    chatid = message.chat.id 
+    admin_ids = [admin.user.id for admin in bot.get_chat_administrators(chatid)]
+
+    if id in admin_ids:
+        claim_chat(chatid, id)
+        bot.reply_to(message, "You have become the master of this chat!")
+    else:
+        bot.reply_to(message, "You are not an admin")
+
+
+#------------------#
+
+## MAIN MESSAGE HANDLER FOR CA
+@bot.message_handler(is_CA=True)
+def caMsg(message):
+    chat_id = message.chat.id
+    owner_id = find_owner(chat_id)
+    ca = findCA(message)
+
+    if view(owner_id)[0] != None:
+        # Build the bullx referral link
+        startString = "https://neo.bullx.io/terminal?chainId=1399811149&address="
+        endString = "&r" + find_bull(chat_id) + "=en"
+        bull_link = f"{startString}{ca}{endString}"
+        escaped_bull_link = bull_link.replace("-", "\\-").replace("_", "\\_").replace(".", "\\.").replace(":", "\\:").replace("?", "\\?").replace("=", "\\=").replace("&", "\\&")
+
+    if view(owner_id)[1] != None:
+        # Build the Axiom referral link 
+        a_startString = "https://axiom.trade/t/"
+        a_endString = "/" + find_ax(chat_id)
+        axiom_link = f"{a_startString}{ca}{a_endString}"
+        escaped_axiom_link = axiom_link.replace("-", "\\-").replace("_", "\\_").replace(".", "\\.").replace(":", "\\:").replace("?", "\\?").replace("=", "\\=").replace("&", "\\&")
+    
+    if view(owner_id)[0] != None and view(owner_id)[1] != None:
+        bot.reply_to(
+            message,
+            f"Here's the token you're looking for: \n \n 🏎*[BullX]({escaped_bull_link})*  \\|  *[Axiom]({escaped_axiom_link})*🏎\n\n`{ca}`",
+            parse_mode="MarkdownV2"
+        )
+
+    elif view(owner_id)[0] != None and view(owner_id)[1] == None:
+        bot.reply_to(
+            message,
+            f"Here's the token you're looking for: \n \n 🏎*[BullX]({escaped_bull_link})*🏎\n\n`{ca}`",
+            parse_mode="MarkdownV2"
+        )
+
+    elif view(owner_id)[0] == None and view(owner_id)[1] != None:
+        bot.reply_to(
+            message,
+            f"Here's the token you're looking for: \n \n 🏎*[Axiom]({escaped_axiom_link})*🏎\n\n`{ca}`",
+            parse_mode="MarkdownV2"
+        )
+
+
+bot.infinity_polling()
---
+++ db.py
@@ -0,0 +1,76 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="acung.mysql.pythonanywhere-services.com",
+    user="acung",
+    password="Kakakaka1",
+    database="acung$lambo_bot"
+)
+
+cursor = conn.cursor()
+
+def add_bull(telegram_id, bref_code):
+    query = "INSERT INTO user_codes (telegram_id, bull_ref_code) VALUES (%s, %s) ON DUPLICATE KEY UPDATE    bull_ref_code = VALUES (bull_ref_code)"
+    values = (telegram_id, bref_code)
+    cursor.execute(query, values)
+    conn.commit()
+
+def add_axiom(telegram_id, aref_code):
+    query = "INSERT INTO user_codes (telegram_id, axiom_ref_code) VALUES (%s, %s) ON DUPLICATE KEY UPDATE    axiom_ref_code = VALUES (axiom_ref_code)"
+    values = (telegram_id, aref_code)
+    cursor.execute(query, values)
+    conn.commit()
+
+def claim_chat(xchat_id, user_id):
+    query = "INSERT INTO chat_owners (chat_id, telegram_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE    telegram_id = VALUES(telegram_id)"
+    values = (xchat_id, user_id)
+    cursor.execute(query, values)
+    conn.commit()
+
+def view(tg_id):
+    query = "SELECT bull_ref_code, axiom_ref_code FROM user_codes WHERE telegram_id = %s"
+    cursor.execute(query, (tg_id,))
+    result = cursor.fetchone()
+    return result
+
+def delcode(tg_id, platform):
+    if platform == "bull_ref_code":
+        query = "UPDATE user_codes SET bull_ref_code = NULL WHERE telegram_id = %s"
+        cursor.execute(query, (tg_id,))
+        conn.commit()
+    elif platform == "axiom_ref_code":
+        query = "UPDATE user_codes SET axiom_ref_code = NULL WHERE telegram_id = %s"
+        cursor.execute(query, (tg_id,))
+        conn.commit()
+
+def find_bull(chat_id):
+    query = "SELECT telegram_id FROM chat_owners WHERE chat_id = %s"
+    cursor.execute(query, (chat_id,))
+    tgid = cursor.fetchone()
+    if tgid is None:
+        return None
+    tgid = tgid[0]
+
+    query = "SELECT bull_ref_code FROM user_codes WHERE telegram_id = %s"
+    cursor.execute(query, (tgid,))
+    result = cursor.fetchone()
+    return result[0]
+
+def find_ax(chat_id):
+    query = "SELECT telegram_id FROM chat_owners WHERE chat_id = %s"
+    cursor.execute(query, (chat_id,))
+    tgid = cursor.fetchone()
+    if tgid is None:
+        return None
+    tgid = tgid[0]
+
+    query = "SELECT axiom_ref_code FROM user_codes WHERE telegram_id = %s"
+    cursor.execute(query, (tgid,))
+    result = cursor.fetchone()
+    return result[0]
+
+def find_owner(chat_id):
+    query = "SELECT telegram_id FROM chat_owners WHERE chat_id = %s"
+    cursor.execute(query, (chat_id,))
+    result = cursor.fetchone()
+    return result[0]
